68543ec07cffbc8f4f5d5d80db0fa3813ce4359e
Robin Svensson <robinesvensson@gmail.com>
2024-09-29 22:31:49 +0200

    fix: update linkedin url.

--- src/components/ContactIcons.js
+++ src/components/ContactIcons.js
@@ -1,7 +1,7 @@
 function ContactIcons({textSizeClass = 'text-4xl', spaceXClass = 'space-x-10'}){
     return(
         <div className={`h-12 flex justify-center items-center ${textSizeClass} ${spaceXClass} text-secondary`}>
-            <a target="_blank" href="https://www.linkedin.com/in/robin-svensson2"><i className="fa-brands fa-linkedin"></i></a>
+            <a target="_blank" href="https://www.linkedin.com/in/robin-rydell"><i className="fa-brands fa-linkedin"></i></a>
             <a target="_blank" href="https://github.com/hoprob"><i className="fa-brands fa-github"></i></a>
             {/* <a target="_blank" href="https://discordapp.com/users/884740341754904606"><i className="fa-brands fa-discord"></i></a> */}
             <a target="_blank" href="mailto:robinehrydell@gmail.com"><i className="fa-solid fa-envelope"></i></a>
